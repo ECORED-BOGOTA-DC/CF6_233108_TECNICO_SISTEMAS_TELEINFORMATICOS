d9dbd0550a38f3306cb2bcb43789e474172c5351
manuelTecno <135905393+manuelechavarria@users.noreply.github.com>
2023-11-01 09:33:40 -0500

    Ajustes finales

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -14,7 +14,7 @@
     
     figure.mt-4(data-aos="zoom-in-up")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/ReZ36xn85F0?si=jUlJxi6TkG0sNRN4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
